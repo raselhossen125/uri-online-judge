78943bbffe525256bdabc90cfce6d496e3af6443
rasel-hossen <straselhossen24@gmail.com>
2022-06-19 23:52:20 +0600

    solved

---
+++ average1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    float A,B,result;
+    scanf("%f %f", &A, &B),
+    result = (A*3.5+B*7.5)/(3.5+7.5);
+    printf("MEDIA = %0.5f\n", result);
+}
---
+++ simpleProduct.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    int a,b,result;
+    scanf("%d", &a),
+    scanf("%d", &b),
+    result = a*b;
+    printf("PROD = %d\n", result);
+}
