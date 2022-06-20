1246e5ad0ff8e7ca329eeb1c5bfef9b4a101f3a1
rasel-hossen <straselhossen24@gmail.com>
2022-06-21 00:15:12 +0600

    solved

--- simpleCalculate.c
+++ simpleCalculate.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main() {
-    int pdCode1, pdQuantity1;
-    double pdSalary1, result;
-    scanf("%d %d %lf", &pdCode1, &pdQuantity1, pdSalary1); 
-    result = pdQuantity1*pdSalary1;
-    printf("res = %0.2lf", );
+    int pdCode1,pdCode2, pdQuantity1,pdQuantity2;
+    double pdSalary1,pdSalary2;
+    scanf("%d %d %lf", &pdCode1, &pdQuantity1, &pdSalary1);
+    scanf("%d %d %lf", &pdCode2, &pdQuantity2, &pdSalary2);
+    printf("VALOR A PAGAR: R$ %0.2lf\n", (pdQuantity1*pdSalary1)+(pdQuantity2*pdSalary2));
 }
